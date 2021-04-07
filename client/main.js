59c89fd4393d8e3d9ea53e92e807f01deb862db2
Zoe_Hunte <coffeeaddict512>
2021-04-07 05:18:17 -0500

    Initial commit

---
+++ client/main.js
@@ -0,0 +1,12 @@
+import { Template } from 'meteor/templating';
+import { ReactiveVar } from 'meteor/reactive-var';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import './main.html';
+import './myAccounts.html';
+import './addProfile.html';
+
+import '../lib/collection.js';
+import './myAccounts.js';
+import './addProfile.js';
