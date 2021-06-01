f17515f635667ae514c793636a8c0f8144a681d5
Zoe_Hunte <coffeeaddict512>
2021-06-01 12:14:32 -0500

    edit profiles

--- client/myAccounts.js
+++ client/myAccounts.js
@@ -23,10 +23,11 @@ Template.myAccounts.events({
         //console.log("deleting", myID);
         //console.log("Deleting document", delID );
     },
-    'click .js-edit'(events){
+    'click .js-edit'(event){
         let myID = this._id;
+        $("#editID").val(myID);
         let imgPath = this.pPic;
-        let fname = this.pfirst;
+        let fname = this.pFirst;
         let lname = this.pLast;
         let age = this.pAge;
         let gender = this.pGen;
@@ -36,5 +37,33 @@ Template.myAccounts.events({
         $('#editlastName').val(lname);
         $('#editage').val(age);
         $('#editModal').modal('show');
+        let genRadio = $('#editModel input[name="genderRadio"]');
+        if (gender === "male")
+            genRadio.filter('[value=male').prop('checked', true);
+        else
+            genRadio.filter('[value=female').prop('checked', true);
+
+    },
+    'click .js-saveEdit'(event){
+        let editID = $("#editID").val();
+        let imgPath = $('#editPic').val();
+        let fname = $('#editfirstName').val();
+        let lname = $('#editlastName').val();
+        let age = $('#editage').val();
+        let gender = $('#editModel input[name="genderRadio"]:checked').val();
+        $('#editModal').modal('hide');
+        //update the document
+        profilesdb.update({_id: editID},
+            {$set:{
+                'pPic': imgPath,
+                'pFirst': fname,
+                'pLast': lname,
+                'pAge': age,
+                'pGen': gender,
+
+            }}
+        );
+
+        //console.log("saving edits", editID);
     }
 });
