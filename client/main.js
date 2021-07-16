c04d2b257a49bbea353082037fd074b0c0d70068
Zoe_Hunte <coffeeaddict512>
2021-07-15 23:56:38 -0500

    added signin faulty temp

--- client/main.js
+++ client/main.js
@@ -4,20 +4,26 @@ import { Session } from 'meteor/session';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-import './main.html';
-import './myAccounts.html';
-import './addProfile.html';
-import './confirmDel.html';
-import './editProfile.html';
-import './filter.html';
-
 
 import '../lib/collection.js';
 import '../lib/userAcct.js';
-import './myAccounts.js';
-import './addProfile.js';
 import './infiniteScroll.js';
+
+import './main.html';
+
+import './myAccounts.html';
+import './myAccounts.js';
+
+import './addProfile.html';
+import './addProfile.js';
+
+import './filter.html';
 import './filter.js';
+
+import './confirmDel.html';
+import './editProfile.html';
+
+import './signIn.html';
 
 //if(Meteor.userId())
     Meteor.subscribe("profData");
